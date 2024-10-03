469e71de1933bc3138bed60382b4e84a45b2345e
Mohammed-Ali Cherraoui <113847479+mo24ali@users.noreply.github.com>
2024-10-03 20:26:47 +0100

    Update Two sum.java

--- Two sum.java
+++ Two sum.java
@@ -1,1 +1,25 @@
 
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            int remainder = target - nums[i];
+            
+            if (map.containsKey(remainder)) {
+                return new int[] { map.get(remainder), i };
+            }
+            
+            
+            map.put(nums[i], i);
+        }
+        
+        
+        return null;
+    }
+}
